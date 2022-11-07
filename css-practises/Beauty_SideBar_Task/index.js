1ed2e9151f46eb7cb8a925e5aeb0606b59e21f87
ShathaAlrayyani7 <shatha.alrayyani@optimumpartners.co>
2022-11-07 15:26:43 +0300

    refactoring the code

--- css-practises/Beauty_SideBar_Task/index.js
+++ css-practises/Beauty_SideBar_Task/index.js
@@ -1,105 +1,119 @@
 "use strict"
 
-class SideBar{
+class NavItem{
     constructor(url,img){
         this.nav = document.createElement('a');
         let icon = document.createElement('img');
-
-        this.nav.setAttribute('class','nav');
-        this.nav.setAttribute('href',`${url}`)
-        icon.setAttribute('class','icons');
-        icon.setAttribute('src',`${img}`)
-        
+        this.nav.setAttribute('class', 'nav');
+        this.nav.setAttribute('href', `${url}`)
+        icon.setAttribute('class', 'icons');
+        icon.setAttribute('src', `${img}`)
         this.nav.appendChild(icon);
-        console.log(this.nav);
-        
-        
     }
     
-    get nav () {
+    get getNav() {
         return this.nav
     }
     
 }
 
-class Container{
-    constructor(){
-        let url = 'https://4kwallpapers.com/images/wallpapers/naruto-digital-art-black-background-amoled-3840x2160-5056.png'
-        let body = document.getElementsByTagName('body')[0];
-        let container = document.createElement('div');
-        let section = document.createElement('div');
-        let background = document.createElement('img');
-        let sideBar = document.createElement('div');
-        
-        container.setAttribute('class','container');
-        section.setAttribute('class','section');
-        sideBar.setAttribute('id','sideBar');
-        sideBar.setAttribute('class','sideBar');
-        background.setAttribute('class','backgroundImg');
-        background.setAttribute('src',url);
-        background.setAttribute('alt','Background Image');
-        sideBar.addEventListener('mouseover', this.showSideBar)
-        background.addEventListener('mouseover', this.hideSideBar)
 
+class SideBar {
+    constructor() {
+        this.sideBar = document.createElement('div');
+        this.sideBar.setAttribute('class', 'sideBar');
         
-        section.appendChild(background);
-        container.appendChild(section);
-        container.appendChild(sideBar);
-        body.appendChild(container);
-        
-    }
-    
-    showSideBar = ()=> {
-        console.log('hi');
-        let sideBar = document.getElementById('sideBar')
-        sideBar.style.transition = "0.5s";
-        sideBar.style.width = "130px";
-        sideBar.style.background = 'lightblue'
-        sideBar.style.whiteSpace = 'wrap'
-    }
-    
-    hideSideBar = () =>{
-        console.log('done');
-        let sideBar = document.getElementById('sideBar')
-        sideBar.style.overflowX = "hidden";
-        sideBar.style.transition = "0.5s";
-        sideBar.style.width = "0px";
-        sideBar.style.whiteSpace = 'nowrap';
-        sideBar.style.background = 'rgb(4, 3, 16)'
-
-    }
-    
-    renderData = () => {
         let data = [
             {
-                "title":"Home", "url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-naruto-sign-100.png"
-            },{
-                "title":"Add", "url":"https://en.wikipedia.org/wiki/Naruto","img":"./assets/icons8-plus-100.png"
-            },{
-                "title":"Find", "url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-folder-100.png"
-            },{
-                "title":"My Contact", "url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-myspace-100.png"
-            },{
-                "title":"Support","url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-support-100.png"
-            },{ 
-                "title":"About Us", "url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-info-100.png"
-            },{
-                "title":"Settings", "url":"https://en.wikipedia.org/wiki/Naruto", "img":"./assets/icons8-settings-100.png"
+                title: "Home", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-naruto-sign-100.png"
+            }, {
+                title: "Add", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-plus-100.png"
+            }, {
+                title: "Find", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-folder-100.png"
+            }, {
+                title: "My Contact", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-myspace-100.png"
+            }, {
+                title: "Support", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-support-100.png"
+            }, {
+                title: "About Us", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-info-100.png"
+            }, {
+                title: "Settings", url: "https://en.wikipedia.org/wiki/Naruto", img: "./assets/icons8-settings-100.png"
             }
-            
         ]
-        let sideBar = document.getElementById('sideBar');
-        for(let i = 0 ; i < data.length ; i++){
-
-            sideBar.append(new SideBar(data[i].url, data[i].img).getNav());
+        
+        for (let i = 0; i < data.length; i++) {
+            this.sideBar.append(new NavItem(data[i].url, data[i].img).getNav);
         }
 
-        } 
+    }
+    
+    get getSideBar(){
+        return this.sideBar
+    }
+    
 }
 
 
-let web = new Container()
-web.renderData();
+class ImgContainer {
+    constructor() {
+        let url = 'https://4kwallpapers.com/images/wallpapers/naruto-digital-art-black-background-amoled-3840x2160-5056.png'
+        this.section = document.createElement('div');
+        let background = document.createElement('img');
+        this.section.setAttribute('class', 'section');
+        background.setAttribute('class', 'backgroundImg');
+        background.setAttribute('src', url);
+        background.setAttribute('alt', 'Background Image');
+        
+        this.section.appendChild(background);
+        
+    }
+    
+    get getImgContainer(){
+        return this.section
+    }
+    
+}
+
+class Page{
+    
+    constructor(){
+        let body = document.getElementsByTagName('body')[0];
+        let container = document.createElement('div');
+        
+        container.setAttribute('class', 'container');
+        container.addEventListener('mousemove', this.showSideBar);
+        body.appendChild(container);
+    }
+
+    renderPage = () => {
+        let sideBar = new SideBar().getSideBar;
+        let imgContainer = new ImgContainer().getImgContainer
+        let container = document.getElementsByClassName('container')[0];
+        container.appendChild(sideBar);
+        container.appendChild(imgContainer);
+    }
+    
+    showSideBar = (event) => {
+        let sideBar = document.getElementsByClassName('sideBar')[0];
+        const classList = Object.values(sideBar.classList ?? {});
+        const clientX = event
+        const x = document.documentElement.clientWidth
+        let percentage = (x * 10)/100 
+        let width = x - percentage
+        
+        if(clientX.clientX > width){
+            sideBar.classList.add('show')
+        }
+        
+        else{
+            sideBar.classList.remove('show')
+        }
+    }
+
+}
+
+let page = new Page()
+page.renderPage();
 
 
 
