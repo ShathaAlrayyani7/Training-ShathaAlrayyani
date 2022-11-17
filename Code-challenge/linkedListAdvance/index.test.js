46e1644048aa2ffca26beec6c631da5a31d2e521
ShathaAlrayyani7 <shatha.alrayyani@optimumpartners.co>
2022-11-17 11:41:56 +0300

    feat: code challenge : linked list advanced

--- Code-challenge/linkedListAdvance/index.test.js
+++ Code-challenge/linkedListAdvance/index.test.js
@@ -19,8 +19,84 @@
 // list.tail.value;   //yields '5';
 
 
-const LinkedList = () => {
-  // fill me in!
+let LinkedList = () => {
+  this.head = null;
 };
 
-// write methods here!
+
+// LinkedList.prototype.makeNode = function(value) {
+//   let node = {};
+//   node.value = value;
+//   node.next = null;
+//   return node;
+// };
+
+
+LinkedList.prototype.addToTail = function(value) {
+  let newNode = makeNode(value);
+  let curr = this.head
+  if(!curr.value){
+    this.head = newNode;
+  }else{
+    while(curr.next){
+      curr = curr.next;
+      curr.next = newNode;
+    }
+  }
+
+};
+
+LinkedList.prototype.removeHead = function(){
+  let curr = this.head
+  let next = curr.next
+  if(!curr.value){
+    return null;
+  }
+  else{
+    this.head = next
+    next = this.head.next
+  }
+  return curr
+};
+
+LinkedList.prototype.contains = function(val){
+  let curr = this.head 
+  while(curr){
+    if(curr.value == val){
+      return true
+    }
+    curr = curr.next
+  }
+  return false
+}
+
+LinkedList.prototype.toString = function(){
+  // Returns: a string representing all the values in the Linked List, formatted as:
+  //"{ a } -> { b } -> { c } -> null"
+  let linked_list = ''
+  const start = '{'
+  const end = '} -> '
+  curr = this.head
+  while(curr){
+    linked_list += `${start} ${curr.value} ${end}`
+    curr = curr.next
+  }
+  linked_list += 'null'
+  return linked_list
+}
+
+let node = new LinkedList();
+
+// let one = node.makeNode('a');
+// let two = node.makeNode('b');
+// let three = node.makeNode('c');
+
+node.addToTail('one')
+console.log(node.toString());
+
+node.addToTail('two')
+console.log(node.toString());
+
+node.addToTail('three');
+console.log(node.toString());
+
